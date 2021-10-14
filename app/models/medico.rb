03315360f8b879e9943ae7f133cf463d8e11967b
unknown <antoniodurvalc@gmail.com>
2021-10-14 10:15:59 -0300

    adicionando o cucumber

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -2,6 +2,7 @@ class Medico < ApplicationRecord
   validates :nome, presence: true, length: {minimum: 3}
   validates :cpf, presence: true, numericality: true, uniqueness: true
   validates :email, uniqueness: true
+  validates :especialidade, presence: true
   validates :crm, uniqueness: true, presence: true
   has_many :consultums
   has_many :pacientes, through: :consultums
