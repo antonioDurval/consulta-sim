d7f26d252146011a6f2849a017d09c8b0d9c541c
unknown <antoniodurvalc@gmail.com>
2021-10-15 18:07:01 -0300

    Adicionando o validates da consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,6 @@
 class Consultum < ApplicationRecord
+  validates :data, presence: true, length: {minimum: 10}
+  validates :hora, presence: true, length: {maximum: 5}
   belongs_to :paciente
   belongs_to :medico
 end
