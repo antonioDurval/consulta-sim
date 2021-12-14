df75b7eecc9149c6d88748f9c4f33c752a4cc65d
unknown <antoniodurvalc@gmail.com>
2021-12-13 23:15:21 -0300

    test model de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar medico valido" do
+    medico = Medico.new(nome:"Dr. Antoim", cpf:"752.187.800-06", email:"antoim@gmail.com", especialidade:"cardiologista", crm:"87897879")
+    assert medico.save
+  end
+
+  test "criar medico invalido" do
+    medico = Medico.new(nome:"", cpf:"752.187.800-06", email:"antoim@gmail.com", especialidade:"cardiologista", crm:"87897879")
+    assert_not medico.save
+  end
+
+  test "criar medico sem email" do
+    medico = Medico.new(nome:"Antoimm", cpf:"752.187.800-06", email:"", especialidade:"cardiologista", crm:"87897879")
+    assert_not medico.save
+  end
+
+  test "criar medico sem crm" do
+    medico = Medico.new(nome:"Antoimm", cpf:"752.187.800-06", email:"antoim@gmail.com", especialidade:"cardiologista", crm:"")
+    assert_not medico.save
+  end
 end
